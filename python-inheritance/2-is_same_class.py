4a8212c3f640963c74c54ed42020cb4556fd4837
AliceUwase <a.uwase3@alustudent.com>
2024-11-08 12:39:30 -0800

    moved files

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# a function that returns True if the object is exactly an instance 
+# a function that returns True if the object is exactly an instance
 """ a function that validates the object"""
 
 
