3affb68afc28a360feaab4cbeb7e3b38ab354fb6
AliceUwase <a.uwase3@alustudent.com>
2024-11-08 11:23:06 -0800

    Task 1:My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# a class MyList that inherits from list
+"""define a class MyList"""
+
+
+class Mylist:
+    """implemented sorted list"""
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
