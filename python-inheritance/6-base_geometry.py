e0b3854304cba00bf84975442970f912ebc9427d
AliceUwase <a.uwase3@alustudent.com>
2024-11-08 13:55:13 -0800

    Task 6:Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# a class BaseGeometry (based on '5-base_geometry.py')
+"""define class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+        Representation of 'BaseGeometry'
+    """
+
+    def area(self):
+        """
+            raises Exception with message
+        """
+        raise Exception('area() is not implemented')
