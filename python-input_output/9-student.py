e6595e1373f736c579a43bf9fb07149ffb2335db
AliceUwase <a.uwase3@alustudent.com>
2024-11-13 06:32:03 -0800

    Task 9:Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
-# function that returns the dictionary description with simple,
-# data structure (list, dictionary, string, integer and boolean),
-# for JSON serialization of an object
+# class 'Student' that defines a student by:
+#   - Public instance attributes:
+#       - first_name
+#       - last_name
+#       - age
+#   - Instantiation with first_name, last_name and age:
+#       def __init__(self, first_name, last_name, age)
+#   - Public method def to_json(self):
+#       that retrieves a dictionary representation,
+#       of a Student instance (same as 8-class_to_json.py)
 """
-    define a function 'class_to_json'
+    class 'Student'
 """
 
 
-def class_to_json(obj):
+class Student:
     """
-        return dictionary description for JSON object
+        define class 'Student'
     """
 
-    return obj.__dict__
+    def __init__(self, first_name, last_name, age):
+        """
+            init a new Student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+            retrieve dictionary representation of a Student instance
+        """
+
+        return self.__dict__
